aacc39c64c9c203406ea98bb66cdfe1b6b7c26f4
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2019-12-08 16:16:57 +0200

    few fixes

--- Mytxtfind.h
+++ Mytxtfind.h
@@ -3,14 +3,8 @@
 
 	int Getline(char s[], FILE *f);
 
-	int getword(char w[], char s[]);
-
 	int substring(char *str1, char *str2);
 
 	int similar (char *s, char *t, int n);
-
-	void print_lines(char *str);
-
-	void print_similar_words(char *str);
 
 	#endif
--- main.c
+++ main.c
@@ -5,51 +5,55 @@
 #define WORD 30
 int main(){
 
-char *mode = "r";
-FILE *f = fopen("input.txt", mode);
-if (f==NULL){ printf("can't open file !@#!@#"); }
+	char *mode = "r";
+	FILE *f = fopen("input.txt", mode);
+	if (f==NULL){
+		printf("can't open file !@#!@#"); 
+	}
 
-char s[LINE];
-char word[WORD];
-char func;
-Getline(s, f);
+	char s[LINE];
+	char word[WORD];
+	char func;
+	Getline(s, f);
 
-int i=0;
-while (s[i]!=' '){ word[i]=s[i]; i++; }
-word[i]='\0';
-while (s[i]!='\0'){ 
-if (s[i]=='a'){ func='a'; }
-if (s[i]=='b'){ func='b'; }
-i++;
-}
-//printf("Word is: %s\nProgram is: %c\n\n", word, func);
-Getline(s, f);
+	int i=0;
+	while (s[i]!=' '){ 
+		word[i]=s[i]; i++; 
+	}
+	word[i]='\0';
+	while (s[i]!='\0'){ 
+		if (s[i]=='a'){ func='a'; }
+		if (s[i]=='b'){ func='b'; }
+		i++;
+	}
+	//printf("Word is: %s\nProgram is: %c\n\n", word, func);
+	Getline(s, f);
 
-if (func=='a'){
-while (fgets(s, LINE, f)>0) {
-if (substring(s, word)) { printf("%s", s); }
-}
-}
+	if (func=='a'){
+		while (fgets(s, LINE, f)>0) {
+			if (substring(s, word)) { printf("%s", s); }
+		}
+	}
 
-if (func=='b'){
-char t[WORD];
-while (fgets(s, LINE, f)>0) {
-i=0;
-int j=0;
-while (s[i]!='\0'){ 
-if (s[i]==' ' || s[i]=='\t' || s[i]=='\n'){ 
-t[j]='\0'; 
-if (similar(t, word, 1)) { printf("%s\n", t); } 
-if (similar(t, word, 0)) { printf("%s\n", t); } 
-j=0; 
-}
-else { t[j]=s[i]; j++; }
-i++;
-}
+	if (func=='b'){
+		char t[WORD];
+		while (fgets(s, LINE, f)>0) {
+			i=0;
+			int j=0;
+			while (s[i]!='\0'){ 
+				if (s[i]==' ' || s[i]=='\t' || s[i]=='\n'){ 
+					t[j]='\0'; 
+					if (similar(t, word, 1)) { printf("%s\n", t); } 
+					if (similar(t, word, 0)) { printf("%s\n", t); } 
+					j=0; 
+				}
+				else { t[j]=s[i]; j++; }
+				i++;
+			}
 
-}
-}
+		}
+	}
 
-fclose(f);
-return 0;
+	fclose(f);
+	return 0;
 }
