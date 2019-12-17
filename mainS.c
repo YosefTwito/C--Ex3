272ebeb2dcd33d74bbc740b79242b454221321ee
YosefTwito <57401163+YosefTwito@users.noreply.github.com>
2019-12-17 15:17:43 +0200

    delete extra output for the tutor.

--- mainS.c
+++ mainS.c
@@ -7,31 +7,21 @@ int s=0;
 int main(){
 
         int arr[size];
-	printf("****** Insertion sort ****** \n\nPlease insert %d numbers:\n" , size);
        
         for(int i=0 ; i<size; i++){
         	scanf("%d" ,&s);
 		*(arr+i)=s;
 	}
 
-    
-	printf("\nOriginal array:\n");
-	int x;
-	for(x=0; x<size-1; x++){
-		printf("%d,", *(arr+x));
-	}
-	printf("%d\n", *(arr+x)); 
-    
-	//shift_element(arr, 3);
 	int n = sizeof(arr)/sizeof(int);
 	insertion_sort(arr, n);
 
-	printf("\nSorted array:\n");
+	int x=0;
 	for(x=0; x<size-1; x++){
 		printf("%d,", *(arr+x));
 	}
 
-	printf("%d\n", *(arr+x));
+	printf("%d", *(arr+x));
 
 return 0;
 }
